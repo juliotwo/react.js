53362e9931dbc964f79b97ebe9c4784a15e708c0
juliotwo1996 <juliotwo1996@gmail.com>
2018-10-16 23:20:57 -0500

    hoo

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import HomeLayout from '../components/home-layout';
 import Categories from '../../categories/components/categories.js'
 import Related from '../components/related.js'
+import ModalContainer from '../../widgets/containers/modal.js'
 class Home extends Component {
   render() {
     return (
       <HomeLayout >
       	<Related/>
 		<Categories categories={this.props.data.categories}/>
+		<ModalContainer>
+		<h1>Esto es un portal</h1>
+		</ModalContainer>
        </HomeLayout >
     )
   }
